e3849d1b2264e0fb3f196680a4f21a5fa34a0965
Heyjuke58 <H_Hinrichs@gmx.de>
2022-02-19 17:16:52 +0100

    beginning of sac
    Co-authored-by: kwie98 <kwie98@users.noreply.github.com>

--- src/sac.py
+++ src/sac.py
@@ -1,56 +1,76 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.nn.functional import relu
+from torch import Tensor, tanh
+from torch.distributions.normal import Normal
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# TODO?
-class Pi(nn.Module):
-    def __init__(self, state_dim: int, hidden_dim: int, action_dim: int, max_action: float):
-        super(Pi, self).__init__()
+
+class SAC:
+    def __init__(self, action_dim: int, state_dim: int, hidden_dim: int, max_action: int) -> None:
+        self.v = Value(state_dim, hidden_dim)
+        self.qf1 = Q(state_dim, action_dim, hidden_dim)
+        self.qf2 = Q(action_dim, action_dim, hidden_dim)
+        self.policy = Policy(action_dim, state_dim, hidden_dim, max_action)
+        self.replay_buffer = []
+
+
+class Policy(nn.Module):
+    # Gaussian policy
+    # For each action in the action space, output a scalar in [-1, 1]
+    def __init__(self, action_dim: int, state_dim: int, hidden_dim: int, max_action: float):
+        super(Policy, self).__init__()
 
         self.l1 = nn.Linear(state_dim, hidden_dim)
-        self.l2 = nn.Linear(hidden_dim, action_dim)
+        self.l2 = nn.Linear(hidden_dim, hidden_dim)
+        # hidden_dim -> mu0, mu1, ..., sig0, sig1, ...
+        self.l3 = nn.Linear(hidden_dim, action_dim * 2)
 
+        self.action_dim = action_dim
         self.max_action = max_action
 
-    def forward(self, state):
-        a = F.relu(self.l1(state))
-        return self.max_action * torch.tanh(self.l2(a))
+    def forward(self, state: Tensor, action: Tensor, deterministic: bool) -> Tensor:
+        h = relu(self.l1(state))
+        h = relu(self.l2(h))
+        h = self.l3(h)
+
+        mus = h[:self.action_dim]
+        sigmas = h[self.action_dim:]
+
+        if deterministic:
+            return self.max_action * tanh(mus)
+        else:
+            action = Normal(mus, sigmas).rsample()
+            return self.max_action * tanh(action)
 
 
-# TODO
-class Critic(nn.Module):
-    def __init__(self, state_dim, action_dim):
-        super(Critic, self).__init__()
+class Value(nn.Module):
+    # V gets the state, outputs a single scalar (soft value)
+    def __init__(self, state_dim: int, hidden_dim: int):
+        super(Value, self).__init__()
 
-        # Q1 architecture
-        self.l1 = nn.Linear(state_dim + action_dim, 256)
-        self.l2 = nn.Linear(256, 256)
-        self.l3 = nn.Linear(256, 1)
+        self.l1 = nn.Linear(state_dim, hidden_dim)
+        self.l2 = nn.Linear(hidden_dim, hidden_dim)
+        self.l3 = nn.Linear(hidden_dim, 1)
 
-        # Q2 architecture
-        self.l4 = nn.Linear(state_dim + action_dim, 256)
-        self.l5 = nn.Linear(256, 256)
-        self.l6 = nn.Linear(256, 1)
+    def forward(self, state: Tensor) -> Tensor:
+        h = relu(self.l1(state))
+        h = relu(self.l2(h))
+        return self.l3(h)
 
-    def forward(self, state, action):
-        sa = torch.cat([state, action], 1)
 
-        q1 = F.relu(self.l1(sa))
-        q1 = F.relu(self.l2(q1))
-        q1 = self.l3(q1)
+class Q(nn.Module):
+    # Q gets the state and action, outputs a single scalar (state-action value)
+    def __init__(self, action_dim: int, state_dim: int, hidden_dim: int):
+        super(Q, self).__init__()
 
-        q2 = F.relu(self.l4(sa))
-        q2 = F.relu(self.l5(q2))
-        q2 = self.l6(q2)
-        return q1, q2
+        self.l1 = nn.Linear(action_dim + state_dim, hidden_dim)
+        self.l2 = nn.Linear(hidden_dim, hidden_dim)
+        self.l3 = nn.Linear(hidden_dim, 1)
 
-    def Q1(self, state, action):
-        sa = torch.cat([state, action], 1)
-
-        q1 = F.relu(self.l1(sa))
-        q1 = F.relu(self.l2(q1))
-        q1 = self.l3(q1)
-        return q1
+    def forward(self, action: Tensor, state: Tensor) -> Tensor:
+        h = relu(self.l1(torch.cat((action, state), dim=0)))
+        h = relu(self.l2(h))
+        return self.l3(h)
